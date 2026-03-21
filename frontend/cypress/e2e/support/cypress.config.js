c99e8ae0f64e7228809c6c71637f84a80225d5f1
OldBoneBro <alex.nighthammer@yandex.ru>
2026-03-21 21:42:55 +0900

    Delete frontend/cypress/e2e/support directory

--- frontend/cypress/e2e/support/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-    },
-  },
-});
