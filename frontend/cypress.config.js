c8cd8cdbc5306366199c51841ff1a464b6dc08f7
OldBoneBro <alex.nighthammer@yandex.ru>
2026-03-21 21:41:19 +0900

    add supportFile

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
+    supportFile: false,
     setupNodeEvents(on, config) {
     },
   },
